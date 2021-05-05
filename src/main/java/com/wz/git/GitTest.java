e92185ef3fbf6b9910ca01c1f43152080783026b
wz <w18530721549@163.com>
2021-05-05 22:04:03 +0800

    push commit

--- src/main/java/com/wz/git/GitTest.java
+++ src/main/java/com/wz/git/GitTest.java
@@ -8,5 +8,6 @@ public class GitTest {
         System.out.println("hello Git4");
         System.out.println("master Git");
         System.out.println("hot-fix Git4");
+        System.out.println("push test");
     }
 }
